8d8ef22aa2f5e8199402bf29e82f267eb3465007
Leonardo <leonardo.xavier.clear@gmail.com>
2022-01-27 14:58:52 -0300

    Adicionando componentes soma, media e sorteio

---
+++ src/components/Media.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+
+    return(
+        <Card title="Média dos Números" green>
+            <div>
+                <span>Resultado:</span>
+                <strong>5</strong>
+
+        
+            </div>
+           
+        </Card>
+    )
+}
---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,18 @@
+
+import React from 'react'
+import Card from './Card'
+
+export default props => {
+
+    return(
+        <Card title="Sorteio de um Número" purple>
+            <div>
+                <span>Resultado:</span>
+                <strong>8</strong>
+
+        
+            </div>
+           
+        </Card>
+    )
+}
